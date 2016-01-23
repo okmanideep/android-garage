929af0d8fd4eefb48761e3e4d57ab0589f2e3f5a
manidesto <manideepiitb@gmail.com>
2016-01-23 18:58:53 +0530

    Kotlin all the way

---
+++ app/src/main/java/com/manidesto/androidgarage/activity/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.manidesto.androidgarage.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.manidesto.androidgarage.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
